ba4d1e0592d2b20cf10661545b8a2b1732f394e1
reto <reto@apache.org>
2013-02-21 16:03:46 +0100

    Permssion handling: writing as priviledged, make sure everybody can read

--- src/main/java/org/apache/stanbol/example/ResourceResolver.java
+++ src/main/java/org/apache/stanbol/example/ResourceResolver.java
@@ -1,5 +1,9 @@
 package org.apache.stanbol.example;
 
+import java.security.AccessController;
+import java.security.Permission;
+import java.security.PrivilegedAction;
+import java.util.Collections;
 import java.util.Date;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
@@ -10,40 +14,47 @@ import javax.ws.rs.core.UriInfo;
 import org.apache.clerezza.rdf.core.BNode;
 import org.apache.clerezza.rdf.core.MGraph;
 import org.apache.clerezza.rdf.core.UriRef;
-import org.apache.clerezza.rdf.core.access.NoSuchEntityException;
+import org.apache.clerezza.rdf.core.access.EntityAlreadyExistsException;
 import org.apache.clerezza.rdf.core.access.TcManager;
+import org.apache.clerezza.rdf.core.access.security.TcAccessController;
+import org.apache.clerezza.rdf.core.access.security.TcPermission;
 import org.apache.clerezza.rdf.core.impl.PlainLiteralImpl;
-import org.apache.clerezza.rdf.core.impl.SimpleMGraph;
 import org.apache.clerezza.rdf.ontologies.DC;
 import org.apache.clerezza.rdf.ontologies.RDF;
 import org.apache.clerezza.rdf.ontologies.RDFS;
 import org.apache.clerezza.rdf.utils.GraphNode;
 import org.apache.clerezza.rdf.utils.UnionMGraph;
+import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.stanbol.commons.indexedgraph.IndexedMGraph;
 import org.apache.stanbol.commons.web.viewable.RdfViewable;
-import org.apache.stanbol.entityhub.core.impl.SiteManagerImpl;
-import org.apache.stanbol.entityhub.core.query.FieldQueryImpl;
-import org.apache.stanbol.entityhub.model.clerezza.RdfRepresentation;
 import org.apache.stanbol.entityhub.model.clerezza.RdfValueFactory;
-import org.apache.stanbol.entityhub.servicesapi.Entityhub;
 import org.apache.stanbol.entityhub.servicesapi.model.Entity;
 import org.apache.stanbol.entityhub.servicesapi.model.Representation;
-import org.apache.stanbol.entityhub.servicesapi.query.FieldQuery;
-import org.apache.stanbol.entityhub.servicesapi.query.QueryResultList;
 import org.apache.stanbol.entityhub.servicesapi.site.SiteManager;
+import org.osgi.service.component.ComponentContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * Uses the DbPedia referenced site to resolve entities.
+ * Uses the SiteManager to resolve entities. Every requested is recorded to
+ * a graph. The client gets information and meta-information about the resource
+ * and sees all previous requests for that resource.
  */
 @Component
 @Service(Object.class)
 @Property(name="javax.ws.rs", boolValue=true)
 @Path("example-service")
 public class ResourceResolver {
+    
+    /**
+     * Using slf4j for normal logging
+     */
+    private static final Logger log = LoggerFactory.getLogger(ResourceResolver.class);
     
     /**
      * This service allows to get entities from configures sites
@@ -60,7 +71,29 @@ public class ResourceResolver {
     /**
      * This is the name of the graph in which we "log" the requests
      */
-    private UriRef LOG_GRAPH_NAME = new UriRef("http://example.org/resource-resolver-log.graph");
+    private UriRef REQUEST_LOG_GRAPH_NAME = new UriRef("http://example.org/resource-resolver-log.graph");
+    
+    @Activate
+    protected void activate(ComponentContext context) {
+        log.info("The example service is being activated");
+        try {
+            tcManager.createMGraph(REQUEST_LOG_GRAPH_NAME);
+            //now make sure everybody can read from the graph
+            //or more precisly, to anybody who can read the content-graph
+            TcAccessController tca = new TcAccessController(tcManager);
+            tca.setRequiredReadPermissions(REQUEST_LOG_GRAPH_NAME, 
+                    Collections.singleton((Permission)new TcPermission(
+                    "urn:x-localinstance:/content.graph", "read")));
+        } catch (EntityAlreadyExistsException ex) {
+            log.debug("The graph for the request log already exists");
+        }
+        
+    }
+    
+    @Deactivate
+    protected void deactivate(ComponentContext context) {
+        log.info("The example service is being activated");
+    }
     
     /**
      * This method return an RdfViewable, this is an RDF serviceUri with associated
@@ -78,7 +111,7 @@ public class ResourceResolver {
         final MGraph responseGraph = new IndexedMGraph();
         //A union graph containing both the response specif triples as well 
         //as the log-graph
-        final UnionMGraph resultGraph = new UnionMGraph(responseGraph, getLogGraph());
+        final UnionMGraph resultGraph = new UnionMGraph(responseGraph, getRequestLogGraph());
         //This GraphNode represents the service within our result graph
         final GraphNode node = new GraphNode(serviceUri, resultGraph);
         //The triples will be added to the first graph of the union
@@ -94,22 +127,6 @@ public class ResourceResolver {
         return new RdfViewable("ServiceEntry", node, ResourceResolver.class);
     }
     
-    @GET
-    @Path("debug")
-    public String serviceEntry() throws Exception {
-        //Entity entity = entityhub.getEntity("http://www.products.com/Jewellery");
-        Entity entity = siteManager.getEntity("http://dbpedia.org/resource/Paris");
-        MGraph mGraph = new IndexedMGraph();
-        RdfValueFactory valueFactory = new RdfValueFactory(mGraph);
-        RdfRepresentation rdfRepresentation = valueFactory.toRdfRepresentation(entity.getRepresentation());
-        UriRef entityUri = null;//rdfRepresentation.getNode();
-        //rdfRepresentation.getRdfGraph().equals(mGraph);
-        return "Entiy: "+entityUri+" in Graph: "+mGraph+" check "+rdfRepresentation.getRdfGraph().equals(mGraph);
-        //FieldQuery query = siteManager.getQueryFactory().createFieldQuery();
-        //FieldQuery query = new FieldQueryImpl();
-        //final QueryResultList<Representation> result = siteManager.find(query);
-        //return "executing query result: "+result.results();
-    }
 
     /**
      * Add the description of a serviceUri to the specified MGraph using SiteManager.
@@ -134,26 +151,32 @@ public class ResourceResolver {
     /**
      * Logs a request to the log-graph
      */
-    private void logRequest(UriRef iri, String userAgent) {
-        MGraph logGraph = getLogGraph();
-        GraphNode loggedRequest = new GraphNode(new BNode(), logGraph);
-        loggedRequest.addProperty(RDF.type, Ontology.LoggedRequest);
-        loggedRequest.addPropertyValue(DC.date, new Date());
-        loggedRequest.addPropertyValue(Ontology.userAgent, userAgent);
-        loggedRequest.addProperty(Ontology.requestedEntity, iri);
+    private void logRequest(final UriRef iri, final String userAgent) {
+        //writing to a persistent graph requires some special permission
+        //by executing the code in a do-priviledged section
+        //the user doesn't need this permissions, anonymous users are thus not
+        //asked to log in
+        AccessController.doPrivileged(new PrivilegedAction<Object>() {
+            public Object run() {
+                final MGraph logGraph = getRequestLogGraph();
+                GraphNode loggedRequest = new GraphNode(new BNode(), logGraph);
+                loggedRequest.addProperty(RDF.type, Ontology.LoggedRequest);
+                loggedRequest.addPropertyValue(DC.date, new Date());
+                loggedRequest.addPropertyValue(Ontology.userAgent, userAgent);
+                loggedRequest.addProperty(Ontology.requestedEntity, iri);
+                return null;
+            }
+        });
+        
     }
 
     /**
-     * This either returns the existing MGraph for the log or creates a new one.
+     * This returns the existing MGraph for the log .
      * 
      * @return the MGraph to which the requests are logged
      */
-    private MGraph getLogGraph() {
-        try {
-            return tcManager.getMGraph(LOG_GRAPH_NAME);
-        } catch (NoSuchEntityException ex) {
-            return tcManager.createMGraph(LOG_GRAPH_NAME);
-        }
+    private MGraph getRequestLogGraph() {
+        return tcManager.getMGraph(REQUEST_LOG_GRAPH_NAME);
     }
     
 }
